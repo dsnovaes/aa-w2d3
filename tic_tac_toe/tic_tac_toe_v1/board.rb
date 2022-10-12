ff1b7df7e2fb9f07223bb7d5e6d5f801679b2115
Diego Novaes <dsnovaes1@gmail.com>
2022-10-12 11:26:15 -0700

    finished board.rb

--- tic_tac_toe/tic_tac_toe_v1/board.rb
+++ tic_tac_toe/tic_tac_toe_v1/board.rb
@@ -46,19 +46,57 @@ class Board
 
     def print
         puts
-        @board.each { |row| puts row.join("|") }
+        @board.each { |row| puts row.join(" | ") }
         puts
     end
 
     def win_row?(mark)
-        @board.each { |row| return true if row.count(mark) == 2 }
+        @board.each { |row| return true if row.count(mark) == 2 && row.count("_") == 1 }
         false
     end
 
     def win_col?(mark)
-        @board.transpose.each { |row| return true if row.count(mark) == 2 }
+        @board.transpose.each { |col| return true if col.count(mark) == 2 && col.count("_") == 1 }
+        false
+    end
+
+    def win_diagonal?(mark)
+        diagonal1 = []
+        diagonal2 = []
+        (0...@board.length).each do |i|
+            diagonal1 << @board[i][i]
+            diagonal2 << @board[i][@board.length - i - 1]
+        end
+        if (diagonal1.count(mark) == 2 || diagonal2.count(mark) == 2) && (diagonal1.count("_") == 1 || diagonal2.count("_") == 1)
+            return true 
+        else
+            false
+        end
+    end
+
+    def win?(mark)
+        if win_row?(mark) || win_col?(mark) || win_diagonal?(mark)
+            true
+        else
+            false
+        end
+    end
+
+    def empty_positions?
+        return true if @board.flatten.count("_") > 0
         false
     end
 
 end
 
+g = Board.new
+g.place_mark([1,1],:X)
+p g.empty_positions?
+g.place_mark([0,1],:O)
+p g.empty_positions?
+g.place_mark([0,2],:X)
+p g.empty_positions?
+g.place_mark([1,0],:X)
+g.print
+p g.win?(:X)
+p g.empty_positions?
