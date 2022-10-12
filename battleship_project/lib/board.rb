c2f43b447595231908798d315f7c233db4177dd8
Diego Novaes <dsnovaes1@gmail.com>
2022-10-11 23:14:57 -0700

    done until hidden_ships_grid

--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -1,3 +1,61 @@
 class Board
-  
+
+    attr_reader :size
+
+    def initialize(n)
+        @grid = Array.new(n) { Array.new(n, :N) }
+        @size = n*n
+    end
+
+    def [](position)
+        @grid[position.first][position.last]
+    end
+
+    def []=(position,value)
+        @grid[position.first][position.last] = value
+    end
+
+    def num_ships
+        result = Hash.new(0)
+        @grid.flatten.each { |k,v| result[k] += 1 }
+        result[:S]
+    end
+
+    def attack(position)
+        aim = self[position]
+        if aim == :S
+            self[position] = :H
+            p "you sunk my battleship!"
+            true
+        else
+            self[position] = :X
+            false
+        end
+    end
+
+    def place_random_ships
+        count = 0
+        ratio = @grid.flatten.count * 0.25
+        until count == ratio
+            max = @grid.first.length
+            coord_x = rand(0...max)
+            coord_y = rand(0...max)
+            spot = [coord_x,coord_y]
+            if self[spot] != :S
+                self[spot] = :S
+                count += 1
+            end
+        end
+    end
+
+    def hidden_ships_grid
+        @grid.map do |subArr|
+            subArr.map { |ele| ele == :S ? :N : ele }
+        end
+    end
+
+    def print_grid(arr)
+        
+    end
+
 end
